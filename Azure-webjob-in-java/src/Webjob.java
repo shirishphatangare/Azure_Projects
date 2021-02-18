ebfe540d3696d3992e05e11f0b0c2f38c9d33513
shirishphatangare <sphatangare@gmail.com>
2021-02-18 08:23:13 -0600

    Adding Azure webjob example

---
+++ Azure-webjob-in-java/src/Webjob.java
@@ -0,0 +1,15 @@
+
+public class Webjob {
+
+	// Simple Webjob to Print a name 10 times
+	public static void main(String[] args) {
+		
+		for (int i=0;i<10;i++) {
+			
+			System.out.println("SHIRISH My Name Via a Webjob");
+		}
+		
+
+	}
+
+}
